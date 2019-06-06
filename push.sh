0e0f1f3385077c16239b6c17183e4a4ab5dabafe
Geoff Garbers <geoff@garbers.co.za>
2019-06-06 13:07:13 +0200

    Add extra newline.

--- push.sh
+++ push.sh
@@ -14,6 +14,7 @@ printf "Do you want to create a date-stamped tag? [Y/n]? "
 read confirm
 
 if [[ "$confirm" = "Y" ]] || [[ "$confirm" = "y" ]] || [[ "$confirm" = "" ]]; then
+    printf "\n"
     printf "\e[38;5;116mRecreating tag on remote (may show errors if remote tag doesn't exist)\e[0m\n"
     git tag -d "$tag"
     git push origin :"$tag"
