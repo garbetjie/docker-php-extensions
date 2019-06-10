87828fc9f473b07fc70934eb19ee2cd0bf158b4d
Geoff Garbers <geoff@garbers.co.za>
2019-06-10 06:28:31 +0200

    Remove date-stamped tags.

--- push.sh
+++ push.sh
@@ -2,29 +2,15 @@
 
 project="$1"
 docker_hub_repo="$2"
-tag="$(date "+%Y%m%d")"
 
 # Ensure we have a project id.
 if [[ "$#" -lt 2 ]]; then
-    echo "Usage: GOOGLE_PROJECT_ID DOCKER_HUB_REPO"
+    printf "Usage: $0 \e[4mGCP_PROJECT\e[0m \e[4mDOCKER_HUB_REPO\e[0m\n"
     exit 1
 fi
 
 # Change into this directory.
 cd "$(cd "$(dirname "$0")" && pwd)"
-
-# Update the tags.
-printf "Do you want to create a date-stamped tag? [Y/n]? "
-read confirm
-
-if [[ "$confirm" = "Y" ]] || [[ "$confirm" = "y" ]] || [[ "$confirm" = "" ]]; then
-    printf "\n"
-    printf "\e[38;5;116mRecreating tag on remote (may show errors if remote tag doesn't exist)\e[0m\n"
-    git tag -d "$tag"
-    git push origin :"$tag"
-    git tag -am "Release: $tag" "$tag"
-    git push origin "$tag"
-fi
 
 # Submit the build.
 printf "\n\e[38;5;116mSubmitting build.\e[0m\n"
