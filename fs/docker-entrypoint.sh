0e1eb311c77784a328e89e412e68e44c6ffd9f7b
Geoff Garbers <geoff@garbers.co.za>
2019-11-04 09:56:33 +0200

    Add external starting of New Relic agent.

--- fs/docker-entrypoint.sh
+++ fs/docker-entrypoint.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env sh
 set -e
 
 # Remove NewRelic configuration file if not enabled.
-if [[ "$NEWRELIC_ENABLED" != "true" ]]; then
+if [ "$NEWRELIC_ENABLED" != "true" ]; then
     rm -f "${PHP_INI_DIR}/conf.d/docker-php-ext-newrelic.ini"
 fi
 
 # Remove XDebug configuration if not enabled.
-if [[ "$XDEBUG_ENABLED" != "true" ]]; then
+if [ "$XDEBUG_ENABLED" != "true" ]; then
     rm -f "${PHP_INI_DIR}/conf.d/docker-php-ext-xdebug.ini"
 fi
 
@@ -20,15 +20,15 @@ version() {
 # Update FPM configuration.
 #
 
-if [[ $MAX_CHILDREN -lt 1 ]]; then
+if [ "$MAX_CHILDREN" -lt 1 ]; then
     # See https://github.com/moby/moby/issues/20688#issuecomment-188923858 for why we check memory.limit_in_bytes first.
     # Need to convert it from bytes to KB though.
     cgroups_mem="$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)"
-    meminfo_mem="$(expr "$(grep MemTotal /proc/meminfo | awk '{print $2}')" "*" "1024")"
+    meminfo_mem="$(($(grep MemTotal /proc/meminfo | awk '{print $2}') * 1024))"
 
     # See https://github.com/carlossg/openjdk/blob/e8bfbbc39ef4aea0fcf07ad6dc43bd11993d3f5b/docker-jvm-opts.sh for the
     # logic of comparing cgroup memory and meminfo memory.
-    if [[ $meminfo_mem -gt $cgroups_mem ]]; then
+    if [ "$meminfo_mem" -gt "$cgroups_mem" ]; then
         available_mem="$cgroups_mem"
     else
         available_mem="$meminfo_mem"
@@ -37,15 +37,16 @@ if [[ $MAX_CHILDREN -lt 1 ]]; then
     # Convert B down to MB.
     available_mem="$(expr "$available_mem" "/" 1024 "/" 1024)"
     clean_memory_limit="$(echo "$MEMORY_LIMIT" | grep -oE '[0-9]+')"
-    export MAX_CHILDREN=`expr "${available_mem}" "*" 90 "/" 100 "/" "${clean_memory_limit}"`
+    export MAX_CHILDREN="$((available_mem * 90 / 100 / clean_memory_limit))"
 fi
 
 # Ensure we always have at least one child.
-if [[ "$MAX_CHILDREN" -lt 1 ]]; then
+if [ "$MAX_CHILDREN" -lt 1 ]; then
     export MAX_CHILDREN=1
 fi
 
 # Substitute values in the php-fpm file.
+# shellcheck disable=SC2016
 envsubst '$PM
           $MAX_CHILDREN
           $MIN_SPARE_SERVERS
@@ -57,14 +58,15 @@ envsubst '$PM
 mv /usr/local/etc/php-fpm.conf.tmp /usr/local/etc/php-fpm.conf
 
 # Comment out configuration that isn't available in 7.2
-if [[ "$(version "$PHP_VERSION")" -lt "$(version 7.3.0)" ]]; then
+if [ "$(version "$PHP_VERSION")" -lt "$(version 7.3.0)" ]; then
     sed -i 's/^decorate_workers_output/\;decorate_workers_output/;s/^log_limit/\;log_limit/' /usr/local/etc/php-fpm.conf
 fi
 
 # Substitute values in the PHP ini files.
-for src_file in `find $PHP_INI_DIR -type f -iname '*.ini'`; do
+for src_file in "$PHP_INI_DIR"/**/*.ini /opt/newrelic/newrelic.cfg; do
     temporary_file="${src_file}.tmp"
 
+    # shellcheck disable=SC2016
     envsubst '$DISPLAY_ERRORS
               $ERROR_REPORTING
               $HTML_ERRORS
@@ -74,6 +76,7 @@ for src_file in `find $PHP_INI_DIR -type f -iname '*.ini'`; do
               $MEMORY_LIMIT
               $NEWRELIC_APP_NAME
               $NEWRELIC_AUTORUM_ENABLED
+              $NEWRELIC_DAEMON_PORT
               $NEWRELIC_LABELS
               $NEWRELIC_LICENCE
               $NEWRELIC_RECORD_SQL
@@ -91,7 +94,12 @@ for src_file in `find $PHP_INI_DIR -type f -iname '*.ini'`; do
     mv "$temporary_file" "$src_file"
 done
 
-if [[ $# -lt 1 ]]; then
+# Start the daemon, before executing the main script.
+set +e
+/usr/local/bin/start-newrelic-daemon
+set -e
+
+if [ $# -lt 1 ]; then
     exec php-fpm
 else
     exec "$@"
