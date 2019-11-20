9ca43c323af4e9aa339fcb1374cb5cbc4a84408b
Geoff Garbers <geoff@garbers.co.za>
2019-11-20 11:36:39 +0200

    Change machine type.

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ docker_hub_repo="$2"
 
 # Ensure we have a project id.
 if [[ "$#" -lt 2 ]]; then
-    printf "Usage: $0 \e[4mGCP_PROJECT\e[0m \e[4mDOCKER_HUB_REPO\e[0m\n"
+    printf "Usage: %s \e[4mGCP_PROJECT\e[0m \e[4mDOCKER_HUB_REPO\e[0m\n" "$0"
     exit 1
 fi
 
@@ -16,5 +16,6 @@ cd "$(cd "$(dirname "$0")" && pwd)"
 printf "\n\e[38;5;116mSubmitting build.\e[0m\n"
 gcloud builds submit \
     --project "$project" \
+    --machine-type n1-highcpu-32 \
     --substitutions "_DOCKER_HUB_REPO=${docker_hub_repo}" \
     --config ./cloudbuild.yaml .
