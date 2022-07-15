2519943c607930513946f5a1c48801203107d0df
Geoff Garbers <geoff@garbers.co.za>
2022-07-15 20:32:44 +0200

    Fix build script, and add debugging output.

--- build.sh
+++ build.sh
@@ -2,6 +2,20 @@
 
 set -e -o pipefail
 
-docker build --build-arg PHP_VERSION=8.0.8 -t build/php:"$1" -f ext/$1/Dockerfile --progress plain ext/$1
+docker build --build-arg PHP_VERSION=8.0.8 -t build/php:"$1" -f extensions/$1/Dockerfile --progress plain extensions/$1
 
-docker images | grep -F build/php
+echo ""
+echo "-----------------------------------------"
+echo "Image list:"
+echo "-----------------------------------------"
+docker images | grep -F "build/php" | grep -F "$1"
+
+echo ""
+echo "-----------------------------------------"
+echo "Module listing:"
+echo "-----------------------------------------"
+cat <<EOT | docker build --no-cache -t build/php:testing - &> /dev/null
+FROM php:8.0.8-cli-alpine3.14
+COPY --from=build/php:$1 / /
+EOT
+docker run --rm build/php:testing php -m
