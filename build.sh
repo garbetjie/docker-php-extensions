dc7127aa4b6a7e5dd5f59509b7d966dacfe53c20
Geoff Garbers <geoff@garbers.co.za>
2022-07-15 20:23:26 +0200

    Remove exec

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-exec docker build --build-arg PHP_VERSION=8.0.8 -t build/php:"$1" -f ext/$1/Dockerfile --progress plain ext/$1
+set -e -o pipefail
+
+docker build --build-arg PHP_VERSION=8.0.8 -t build/php:"$1" -f ext/$1/Dockerfile --progress plain ext/$1
 
 docker images | grep -F build/php
