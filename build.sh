50923818a4a7abf32c388ab42dee3f225fe2f441
Geoff Garbers <geoff@garbers.co.za>
2022-07-28 21:59:51 +0200

    Update image size calculation.

--- build.sh
+++ build.sh
@@ -8,16 +8,21 @@ echo ""
 echo "-----------------------------------------"
 echo "Image list:"
 echo "-----------------------------------------"
-docker images | grep -F "build/php" | grep -F "$1"
+docker images --filter reference=build/php:"$1"
 
-echo ""
-echo "-----------------------------------------"
-echo "Module listing:"
-echo "-----------------------------------------"
 cat <<EOT | docker build -t build/php:testing --progress plain - &> /dev/null
 FROM php:8.0.8-cli-alpine3.14
 COPY --from=build/php:$1 / /
 COPY --from=build/php:builder / /
 RUN docker-php-install-dependencies.sh
 EOT
+
+start_size="$(docker image inspect php:8.0.8-cli-alpine3.14 | jq -r '.[0].Size')"
+end_size="$(docker image inspect build/php:testing | jq -r '.[0].Size')"
+echo "Final layer size: $(echo "scale=1; ($end_size - $start_size)" | bc | numfmt --to=iec --round=nearest)"
+
+echo ""
+echo "-----------------------------------------"
+echo "Module listing:"
+echo "-----------------------------------------"
 docker run --rm build/php:testing php -m
