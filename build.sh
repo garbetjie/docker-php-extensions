4f548433df1c4240548bcfbe0fe14f0d58e33c49
Geoff Garbers <geoff@garbers.co.za>
2022-11-21 22:18:49 +0200

    Allow setting of PHP version when building.

--- build.sh
+++ build.sh
@@ -4,15 +4,17 @@ set -e -o pipefail
 
 platform=""
 ext="$1"
+php_version="8.0.22"
 
 while [ $# -gt 0 ]; do
   case "$1" in
     --x86) platform="--platform linux/amd64"; shift;;
+    --php-version) php_version="$2"; shift 2;;
     *) ext="$1"; shift;;
   esac
 done
 
-docker build --build-arg PHP_VERSION=8.0.22 $platform -t build/php:"$ext" -f extensions/$ext/Dockerfile --progress plain extensions/$ext
+docker build --build-arg "PHP_VERSION=$php_version" $platform -t build/php:"$ext" -f extensions/$ext/Dockerfile --progress plain extensions/$ext
 
 echo ""
 echo "-----------------------------------------"
@@ -20,14 +22,11 @@ echo "Image list:"
 echo "-----------------------------------------"
 docker images --filter reference=build/php:"$ext"
 
-cat <<EOT > tmp.Dockerfile
-FROM php:8.0.22-cli-alpine3.16
+cat <<EOT | docker build -t build/php:testing $platform --progress plain -
+FROM php:$php_version-cli-alpine3.16
 COPY --from=build/php:$ext / /
 RUN wget -O- https://raw.githubusercontent.com/garbetjie/docker-php/main/install-dependencies.sh | sh
 EOT
-
-docker build -t build/php:testing $platform --progress plain -f tmp.Dockerfile .
-rm tmp.Dockerfile
 
 start_size="$(docker image inspect php:8.0.8-cli-alpine3.14 | jq -r '.[0].Size')"
 end_size="$(docker image inspect build/php:testing | jq -r '.[0].Size')"
