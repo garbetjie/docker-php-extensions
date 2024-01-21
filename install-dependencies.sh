c833983a1ec1ac3d13e26ea01da14a09ac33cf13
Geoff Garbers <geoff@garbers.co.za>
2024-01-21 20:28:37 +0200

    Remove dependencies sub-directory.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,14 +3,14 @@
 set -xe
 
 # Install APK dependencies.
-if [ -d /docker-php-extensions/dependencies/apk ]; then
+if [ -d /docker-php-extensions/apk ]; then
   echo "### Installing APK dependencies..."
-  awk '{ print $0 }' /docker-php-extensions/dependencies/apk/* | tr '\n' ' ' | xargs apk add --no-cache
+  awk '{ print $0 }' /docker-php-extensions/apk/* | tr '\n' ' ' | xargs apk add --no-cache
 fi
 
 # Run custom setup scripts.
-if [ -d /docker-php-extensions/dependencies/shell ]; then
-  for filename in /docker-php-extensions/dependencies/shell/*; do
+if [ -d /docker-php-extensions/shell ]; then
+  for filename in /docker-php-extensions/shell/*; do
     echo "### Executing extension setup script [$(basename "$filename")]..."
     sh "$filename"
   done
