56e3fc156f5e84eff21fa54004ef492c52d0ce93
Geoff Garbers <geoff@garbers.co.za>
2024-01-22 12:11:41 +0200

    Provide backwards compatibility for installation script.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -8,6 +8,11 @@ if [ -d /docker-php-extensions/apk ]; then
   awk '{ print $0 }' /docker-php-extensions/apk/* | tr '\n' ' ' | xargs apk add --no-cache
 fi
 
+if [ -d /tmp/docker-php-dependencies.d/apk ]; then
+  echo "### Installing APK dependencies..."
+  awk '{ print $0 }' /tmp/docker-php-dependencies.d/apk/* | tr '\n' ' ' | xargs apk add --no-cache
+fi
+
 # Run custom setup scripts.
 if [ -d /docker-php-extensions/shell ]; then
   for filename in /docker-php-extensions/shell/*; do
@@ -16,5 +21,12 @@ if [ -d /docker-php-extensions/shell ]; then
   done
 fi
 
+if [ -d /tmp/docker-php-dependencies.d/shell ]; then
+  for filename in /tmp/docker-php-dependencies.d/shell/*; do
+    echo "### Executing extension setup script [$(basename "$filename")]..."
+    sh "$filename"
+  done
+fi
+
 # Clean up.
-rm -rf /docker-php-extensions
+rm -rf /docker-php-extensions /tmp/docker-php-dependencies.d
