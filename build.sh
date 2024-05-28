64c329e99c9d20898d8ae5ccc5fab650437d7dcd
Geoff Garbers <geoff@garbers.co.za>
2024-05-28 21:49:26 +0200

    Make alpine version part of build scripts.

--- build.sh
+++ build.sh
@@ -5,19 +5,26 @@ set -e -o pipefail
 platform=""
 ext="$1"
 php_version="8.1"
-alpine_version="3.18"
+alpine_version="3.19"
 
 while [ $# -gt 0 ]; do
   case "$1" in
     --x86) platform="--platform linux/amd64"; shift;;
     --arm) platform="--platform linux/arm64"; shift;;
-    --alpine-version) alpine_version="$2"; shift 2;;
     --php-version) php_version="$2"; shift 2;;
     *) ext="$1"; shift;;
   esac
 done
 
-docker build --build-arg "PHP_VERSION=$php_version" --build-arg "ALPINE_VERSION=$alpine_version" $platform -t build/php:"$ext" -f extensions/$ext/Dockerfile --progress plain extensions/$ext
+docker build \
+  --no-cache \
+  -t build/php:"$ext" \
+  --build-arg "PHP_VERSION=$php_version" \
+  --build-arg "ALPINE_VERSION=$alpine_version" \
+  $platform \
+  -f extensions/$ext/Dockerfile \
+  --progress plain \
+  extensions/$ext
 
 echo ""
 echo "-----------------------------------------"
@@ -31,7 +38,7 @@ COPY --from=build/php:$ext / /
 RUN wget -O- https://raw.githubusercontent.com/garbetjie/docker-php/main/install-dependencies.sh | sh
 EOT
 
-start_size="$(docker image inspect php:8.0.8-cli-alpine3.14 | jq -r '.[0].Size')"
+start_size="$(docker image inspect php:$php_version-cli-alpine$alpine_version | jq -r '.[0].Size')"
 end_size="$(docker image inspect build/php:testing | jq -r '.[0].Size')"
 echo "Final layer size: $(echo "scale=1; ($end_size - $start_size)" | bc | numfmt --to=iec --round=nearest)"
 
