42dee4eb49ac38b44e63ec3148097bf2fcf3b1cd
Geoff Garbers <geoff@garbers.co.za>
2025-01-24 22:22:57 +0200

    Update generation script.

--- generate.sh
+++ generate.sh
@@ -14,31 +14,32 @@ if [ -f "extensions/$1/Dockerfile" ]; then
 fi
 
 cat <<EOT > "extensions/$1/Dockerfile"
-ARG PHP_VERSION
-ARG ALPINE_VERSION=3.18
-FROM php:\${PHP_VERSION}-cli-alpine\${ALPINE_VERSION}
+ARG IMAGE_TAG="8.4-cli-bookworm"
+FROM php:\$IMAGE_TAG
 
 # Unpack source
 RUN docker-php-source extract
 
 # Download
 RUN mkdir /usr/src/php/ext/${1}
-RUN wget -O- https://pecl.php.net/get/${1}-VERSION.tgz | tar -C /usr/src/php/ext/${1} -xzf - --strip-components 1
+RUN curl https://pecl.php.net/get/${1}-VERSION.tgz | tar -C /usr/src/php/ext/${1} -xzf - --strip-components 1
 
 # Install
+# RUN apt update
+# RUN apt install -y PACKAGES_TO_INSTALL
 RUN docker-php-ext-configure $1
 RUN docker-php-ext-install $1
 
 # Package
-COPY apk /docker-php-extensions/apk/$1
-COPY shell /docker-php-extensions/shell/$1
+COPY apt /opt/docker-php-extensions/apt/$1
+COPY shell /opt/docker-php-extensions/shell/$1
 RUN tar -cf /tmp/files.tar \\
       \$(php-config --extension-dir)/$1.so \\
       \$(php-config --ini-dir)/docker-php-ext-$1.ini \\
-      /docker-php-extensions
+      /opt/docker-php-extensions
 
 
-FROM php:\${PHP_VERSION}-cli-alpine\${ALPINE_VERSION}
+FROM php:\$IMAGE_TAG
 
 COPY --from=0 /tmp/files.tar /tmp/
 RUN mkdir /tmp/root && tar -xf /tmp/files.tar -C /tmp/root
@@ -51,8 +52,8 @@ EOT
 
 # Create file system
 
-# Make APK dependency file.
-touch "extensions/$1/apk"
+# Make package dependency file.
+touch "extensions/$1/apt"
 
 # Make shell dependency file.
 cat <<EOT > "extensions/$1/shell"
