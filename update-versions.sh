5e8a19d17a3deaf059bdfad54dab0d3d1e4e92b7
Geoff Garbers <geoff@garbers.co.za>
2025-02-26 20:48:58 +0200

    Make update script dynamic.

--- update-versions.sh
+++ update-versions.sh
@@ -10,26 +10,7 @@ if ! command -v pup &> /dev/null; then
   exit 1
 fi
 
-for ext in amqp \
-           apcu \
-           ds \
-           grpc \
-           igbinary \
-           imagick \
-           memcached \
-           memprof \
-           mongodb \
-           msgpack \
-           pdo_sqlsrv \
-           protobuf \
-           redis \
-           sqlsrv \
-           ssh2 \
-           swoole \
-           uopz \
-           xdebug \
-           yaml
-do
+while IFS= read -r ext; do
   echo -n "Fetching latest version for extension [$ext]... "
 
   version="$(curl -s "https://pecl.php.net/package/$ext" | pup 'table.middle td.content > table:nth-of-type(3) tr:nth-child(3) th:first-child a text{}')"
@@ -37,4 +18,4 @@ do
   "$sed" -i "s/https:\/\/pecl.php.net\/get\/$ext-\?.*.tgz/https:\/\/pecl.php.net\/get\/$ext-$version.tgz/" extensions/$ext/Dockerfile
 
   echo "$version [done]"
-done
+done <<< "$(grep -He 'curl https:\/\/pecl\.php\.net' extensions/*/Dockerfile | cut -f1 -d: | xargs -n1 dirname | xargs -n1 basename)"
